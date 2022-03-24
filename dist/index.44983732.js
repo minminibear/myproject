c0fcb32f9c4661061c26a6c81a194379024733da
minminibear <tell020114@gmail.com>
2022-03-24 16:15:15 +0900

    jQuery:フォローボタン

---
+++ dist/index.44983732.js
@@ -0,0 +1,33 @@
+// class名：data-follow="false"でアカウント一人ひとりがすでにフォローしているのかをチェック。
+// そしてtrueの場合：ボタンに対してisFollowクラスを追加する。
+$(function() {
+    $(".js-account-item").each(function() {
+        var $this = $(this);
+        var $thisData = $this.data();
+        if ($thisData.follow) $this.find(".js-follow-btn").addClass("isFollow");
+    });
+    // ボタンをクリックした時にフォローかフォロー解除かを切り替える
+    // data-followがtrueの場合、
+    // ・isFollowクラスを削除
+    // ・ボタンのテキストを「フォローする」にする
+    // ・data-followをfalseに変更する
+    $(".js-follow-btn").on("click", function() {
+        var $this = $(this);
+        var $accountItem = $this.parents(".js-account-item");
+        var $thisData = $accountItem.data();
+        if ($thisData.follow) {
+            //  ajax　(実際のプロダクトではajaxを使用してサーバーサイドにフォロー・解除の変更がされたことを送信しなければいけない)
+            $this.removeClass("isFollow");
+            $this.text("フォローする");
+            $accountItem.data("follow", false);
+        } else {
+            // ajax
+            $this.addClass("isFollow");
+            $this.text("フォロー中");
+            $accountItem.data("follow", true);
+        }
+        console.log($thisData);
+    });
+});
+
+//# sourceMappingURL=index.44983732.js.map
---
+++ dist/index.de5c0784.js
@@ -0,0 +1,6 @@
+const app = document.getElementById("app");
+const p = document.createElement("p");
+p.innerText = "Hello,World!";
+app.appendChild(p);
+
+//# sourceMappingURL=index.de5c0784.js.map
