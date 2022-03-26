432040016ed9369f1f5a83be27f4c26b97ca713e
minminibear <tell020114@gmail.com>
2022-03-26 16:41:17 +0900

    stateからリアルDOMに表示

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -523,14 +523,109 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _createElement = require("./vdom/createElement");
 var _createElementDefault = parcelHelpers.interopDefault(_createElement);
 var _render = require("./vdom/render");
-const view = _createElementDefault.default("p", {
-    attrs: {
-    },
-    children: [
-        "仮想DOMの学習スタート!"
+// 初期状態
+const INITIAL_STATE = {
+    accounts: [
+        {
+            id: 1,
+            name: "リオネル・メッシ",
+            team: "FCバルセロナ",
+            description: "アルゼンチンサンタフェ州ロサリオ出身のイタリア系アルゼンチン人サッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。アルゼンチン代表。ポジションはフォワード (wikipedia)",
+            isFollow: false
+        },
+        {
+            id: 2,
+            name: "クリスティアーノ・ロナウド",
+            team: "Juventus",
+            description: "ポルトガル・フンシャル出身のサッカー選手。セリエA・ユヴェントスFC所属。ポルトガル代表。ポジションはフォワード (wikipedia)",
+            isFollow: false
+        },
+        {
+            id: 3,
+            name: "ネイマール",
+            team: "パリサンジェルマン",
+            description: "ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。ブラジル代表。リーグ・アン・パリ・サンジェルマンFC所属。ポジションはフォワード (wikipedia)",
+            isFollow: false
+        }, 
     ]
-});
-const $app = _render.render(view); //仮想DOMであるviewをrender関数に渡してリアルDOMを作る
+};
+const accountItem = (account)=>{
+    return _createElementDefault.default("div", {
+        attrs: {
+        },
+        children: [
+            _createElementDefault.default("div", {
+                attrs: {
+                    class: "account__summary"
+                },
+                children: [
+                    _createElementDefault.default("div", {
+                        attrs: {
+                        },
+                        children: [
+                            _createElementDefault.default("p", {
+                                attrs: {
+                                    class: "account__name"
+                                },
+                                children: [
+                                    account.name
+                                ]
+                            }),
+                            _createElementDefault.default("p", {
+                                attrs: {
+                                    class: "account__team"
+                                },
+                                children: [
+                                    account.team
+                                ]
+                            }), 
+                        ]
+                    }),
+                    _createElementDefault.default("div", {
+                        attrs: {
+                        },
+                        children: [
+                            _createElementDefault.default("button", {
+                                attrs: {
+                                    type: "button",
+                                    class: `followBtn ${account.isFollow ? "isFollow" : ""}`
+                                },
+                                children: [
+                                    account.isFollow ? "フォロー中" : "フォローする"
+                                ]
+                            }), 
+                        ]
+                    }), 
+                ]
+            }),
+            _createElementDefault.default("p", {
+                attrs: {
+                    class: "account__description"
+                },
+                children: [
+                    account.description
+                ]
+            }), 
+        ]
+    });
+};
+const view = (props)=>_createElementDefault.default("ul", {
+        attrs: {
+            class: "accountList"
+        },
+        children: props.accounts.map((e)=>{
+            return _createElementDefault.default("li", {
+                attrs: {
+                    class: "accountList__item"
+                },
+                children: [
+                    accountItem(e)
+                ]
+            });
+        })
+    })
+;
+const $app = _render.render(view(INITIAL_STATE)); //仮想DOMであるviewをrender関数に渡してリアルDOMを作る
 const el = document.getElementById('app'); // src/index.htmlに書かれている <div id="app"></div> を取得
 el.appendChild($app);
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,101 @@
 import h from "./vdom/createElement";
 import { render } from './vdom/render';
 
-const view = h("p", {
-    attrs: {},
-    children: ["仮想DOMの学習スタート!"],
-});
+// 初期状態
+const INITIAL_STATE = {
+    accounts: [
+        {
+            id: 1,
+            name: "リオネル・メッシ",
+            team: "FCバルセロナ",
+            description:
+            "アルゼンチンサンタフェ州ロサリオ出身のイタリア系アルゼンチン人サッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。アルゼンチン代表。ポジションはフォワード (wikipedia)",
+            isFollow: false,
+        },
+        {
+            id: 2,
+            name: "クリスティアーノ・ロナウド",
+            team: "Juventus",
+            description:
+                "ポルトガル・フンシャル出身のサッカー選手。セリエA・ユヴェントスFC所属。ポルトガル代表。ポジションはフォワード (wikipedia)",
+            isFollow: false,
+        },
+        {
+            id: 3,
+            name: "ネイマール",
+            team: "パリサンジェルマン",
+            description:
+                "ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。ブラジル代表。リーグ・アン・パリ・サンジェルマンFC所属。ポジションはフォワード (wikipedia)",
+            isFollow: false,
+        },
+    ],
+};
 
-const $app = render(view); //仮想DOMであるviewをrender関数に渡してリアルDOMを作る
+const accountItem = (account) => {
+    return h("div", {
+        attrs: {},
+        children: [
+            h("div", {
+                attrs: {
+                    class: "account__summary",
+                },
+                children: [
+                    h("div", {
+                        attrs: {},
+                        children: [
+                            h("p", {
+                                attrs: {
+                                    class: "account__name",
+                                },
+                                children: [account.name],
+                        }),
+                        h("p", {
+                            attrs: {
+                                class: "account__team",
+                            },
+                            children: [account.team],
+                        }),
+                    ],
+                }),
+                h("div", {
+                    attrs: {},
+                    children: [
+                        h("button", {
+                            attrs: {
+                                type: "button",
+                                class: `followBtn ${account.isFollow ? "isFollow" : ""}`,
+                            },
+                            children: [account.isFollow ? "フォロー中" : "フォローする"],
+                        }),
+                    ],
+                }),
+            ],
+        }),
+        h("p", {
+            attrs: {
+                class: "account__description",
+            },
+            children: [account.description],
+        }),
+        ],
+    });
+};
+
+const view = (props) => // stateの変更ができないように明治的にpropsという名前にする
+    h("ul", {
+        attrs: {
+            class: "accountList",
+        },
+        children: props.accounts.map((e) => {
+            return h("li", {
+                attrs: {
+                    class: "accountList__item",
+                },
+                children: [accountItem(e)],
+            });
+        }),
+    });
+
+const $app = render(view(INITIAL_STATE)); //仮想DOMであるviewをrender関数に渡してリアルDOMを作る
 const el = document.getElementById('app');// src/index.htmlに書かれている <div id="app"></div> を取得
-el.appendChild($app)
+el.appendChild($app);
