6f754a0bc59eef7114c74a830712c777f61e2d84
minminibear <tell020114@gmail.com>
2022-03-27 15:54:44 +0900

    dispatcherの作成

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -745,10 +745,29 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "app", ()=>app
 );
 var _render = require("./render");
-const app = ({ root , initialState , view , actions  })=>{
+const app = ({ root , initialState , view , actions: actions1  })=>{
     const $el = document.querySelector(root);
     let newNode = view(initialState);
     // console.log($el,newNode);
+    let state = initialState; //アカウント一覧を代入する
+    // action(今回の場合、toggleFollow)にstateの更新とリアルDOMの更新がされるようにする
+    const dispatcher = function(actions) {
+        const dispatchedActions = {
+        }; //空のオブジェクトを作成
+        //　actionの関数を追加していく作業をループ処理
+        // actions：index.jsに定義したactionsのこと
+        for(const key in actions){
+            const action = actions[key];
+            dispatchedActions[key] = (option)=>{
+                setState(action(state, option));
+                renderDom();
+            };
+        }
+        return dispatchedActions;
+    };
+    const setState = function(newState) {
+        if (state !== newState) state = newState;
+    };
     const renderDom = function() {
         $el.appendChild(_render.render(newNode));
     };
--- src/js/vdom/app.js
+++ src/js/vdom/app.js
@@ -6,6 +6,31 @@ export const app = ({ root, initialState, view, actions }) => {
     let newNode = view(initialState);
     // console.log($el,newNode);
 
+    let state = initialState; //アカウント一覧を代入する
+
+    // action(今回の場合、toggleFollow)にstateの更新とリアルDOMの更新がされるようにする
+    const dispatcher = function (actions) {
+        const dispatchedActions = {}; //空のオブジェクトを作成
+
+        //　actionの関数を追加していく作業をループ処理
+        // actions：index.jsに定義したactionsのこと
+        for (const key in actions ) { 
+            const action = actions[key];
+
+            dispatchedActions[key] = (option) => {
+                setState(action(state, option));
+                renderDom();
+            };
+        }
+        return dispatchedActions;
+    };
+
+    const setState = function (newState) {
+        if (state !== newState) {
+            state = newState;
+        }
+    };
+
     const renderDom = function () {
         $el.appendChild(render(newNode));
     };
